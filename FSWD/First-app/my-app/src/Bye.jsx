6fb6548a4f0e47f22f307a18c151b838c39061cb
kiranprasad230 <kiranprasad2306@gmail.com>
2026-05-21 13:49:44 +0530

    Added my-app

--- FSWD/First-app/my-app/src/Bye.jsx
+++ FSWD/First-app/my-app/src/Bye.jsx
@@ -1,4 +1,30 @@
+// function Bye(){
+//     return <h2>Bye component</h2>
+// }
+// export default Bye
+
+//import styles from "./Bye.module.css"
+
 function Bye(){
-    return <h2>Bye component</h2>
+    const users =[
+        { firstName:"Kiran", lastName:"Prasad", age:20},
+        { firstName:"Ashraf", lastName:"vantiya", age:30},
+        { firstName:"Priya", lastName:"Priya", age:19}
+    ]
+    function fullname(user){
+        return user.firstName + " " + user.lastName;
+    }
+
+    return(
+        <div>
+            <h2>person details</h2>
+            <ul>
+            {users.map((user,index) => (
+                <li>{fullname(user)} is {user.age} years old</li>
+            ))}
+            </ul>
+        </div>
+    )
 }
+
 export default Bye
--- FSWD/First-app/my-app/src/Hello.jsx
+++ FSWD/First-app/my-app/src/Hello.jsx
@@ -1,7 +1,48 @@
-import './App.css'
-function Hello(){
-    const name ="Kiran"
+// //function Hello(){
+//      const getName = (Kiran) => {
+//     return "Kirannn"
+//    }
+//    function handleClick(){
+//    alert("Button was clicked")
+//    }
+//    const handleInput= (event)=>{
+//     console.clear()
+//     console.log("Value : ", event.target.value)
+//    }
+//    const name = "Kiran"
+//    const name1 = "Prasad"
 
-    return <h2 className='test'>Hello {name}</h2>
+//    const handleMouseOver = () => console.log("Mouse is over the text")
+//    const handleDoubleClick= () => console.log("Text double clicked")
+
+//    return(
+//     <>
+//         <h1>Hello {getName(name)}</h1>
+//         <h1>Bye {getName(name1)}</h1>
+
+//         <p onMouseOver={handleMouseOver} onDoubleClick={handleDoubleClick}>Loren rat cat mat hat</p>
+
+//         <button onClick={handleClick}>Click me</button>
+//         <button onClick={()=> alert("Hello from inline function")}>Say hello</button>
+//         <br />
+//         <input type="text" onChange={handleInput} placeholder="type something" />
+//     </>
+//    )
+// }
+// export default Hello
+
+function Hello(){
+    const fruits =["apple","banana","orange"]
+
+    return(
+        <div>
+            <h2>Fruit List</h2>
+            <ul>
+                {fruits.map((fruits, index) => (
+                    <li>{index} - {fruits}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 export default Hello
